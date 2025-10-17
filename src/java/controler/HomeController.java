6e57c6ac969a5b33c04a57c029b8fcb8edb75f30
nguyensang-17 <nguyensang.17062004@gmail.com>
2025-10-17 21:39:43 +0700

    vnpay
    Thêm src vnpay

--- src/java/controler/HomeController.java
+++ src/java/controler/HomeController.java
@@ -3,7 +3,6 @@ package controler;
 import DAO.ProductDAO;
 import models.Product;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "HomeController", urlPatterns = {"/home", ""})
+//@WebServlet(name = "HomeController", urlPatterns = {"/home"})
 public class HomeController extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -25,7 +24,7 @@ public class HomeController extends HttpServlet {
             // Lấy 8 sản phẩm mới nhất để hiển thị trên trang chủ
             List<Product> featuredProducts = productDAO.getFeaturedProducts(8);
             request.setAttribute("featuredProducts", featuredProducts);
-            request.getRequestDispatcher("home.jsp").forward(request, response);
+            request.getRequestDispatcher("/home.jsp").forward(request, response);
         } catch (SQLException e) {
             // Có thể chuyển hướng đến một trang lỗi chung
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Không thể tải dữ liệu trang chủ.");
